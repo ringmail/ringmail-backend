48abc05b8a32851445172dc0b005da7754e9eec7
Note <note@ip-172-31-8-117.us-west-1.compute.internal>
2015-08-25 22:32:05 +0000

    routing changes

--- lib/Page/ring/lookup.pm
+++ lib/Page/ring/lookup.pm
@@ -7,13 +7,13 @@ use vars qw();
 use Moose;
 use JSON::XS 'encode_json';
 use Data::Dumper;
-use HTML::Entities 'encode_entities';
+use URI::Escape;
 use POSIX 'strftime';
 
 use Note::XML 'xml';
 use Note::Param;
 
-use Ring::User;
+use Ring::Route;
 
 extends 'Note::Page';
 
@@ -25,11 +25,40 @@ sub load
 	my $form = $obj->form();
 	::log($form);
 	my $res = 'noentry';
+	my $route = new Ring::Route();
 	my $from = $form->{'from'};
+	$from =~ s/^sip\://;
+	$from =~ s/\@.*//g;
+	my $fru = $route->get_from_user(
+		'phone' => $from,
+	);
+	if (defined $fru)
+	{
+		$from = $fru->{'login'};
+		#$from =~ s/\@/%/;
+		$from = uri_escape($from);
+	}
 	my $to = $form->{'to'};
 	$to =~ s/^sip\://;
 	$to =~ s/\@.*//g;
-	$res = "from=$from;to=$to";
+	$to =~ s/%([0-9A-Fa-f]{2})/chr(hex($1))/eg;
+	my $target = {};
+	if ($to =~ /%/)
+	{
+		$to =~ s/%/@/;
+	}
+	my $type = $route->get_target_type(
+		'target' => $to,
+	);
+	my $dest = $route->get_route(
+		'type' => $type,
+		$type => $to,
+	);
+	if (defined $dest)
+	{
+		$res = "from=$from;to=$dest->{'phone'}";
+	}
+	::log("From: $from", "To: $to", "Type: $type", "Dest: $res");
 	return $res;
 }
 
