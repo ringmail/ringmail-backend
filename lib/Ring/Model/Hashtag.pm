204655b22c35d878dfae21dd8f7784290cb980ae
Arkadiy <arkadiy@dyl.com>
2016-05-05 01:20:30 +0000

    $ perltidy

--- lib/Ring/Model/Hashtag.pm
+++ lib/Ring/Model/Hashtag.pm
@@ -17,133 +17,101 @@ use Ring::User;
 
 no warnings qw(uninitialized);
 
-sub validate_tag
-{
-	my ($obj, $param) = get_param(@_);
-	my $tag = $param->{'tag'};
-	if ($tag =~ /^[a-z0-9_]+$/)
-	{
-		return 1;
-	}
-	return 0;
+sub validate_tag {
+    my ( $obj, $param ) = get_param(@_);
+    my $tag = $param->{'tag'};
+    if ( $tag =~ /^[a-z0-9_]+$/ ) {
+        return 1;
+    }
+    return 0;
 }
 
-sub validate_target
-{
-	my ($obj, $param) = get_param(@_);
-	my $target = $param->{'target'};
-    if ($target =~ /^$RE{URI}{HTTP}{-scheme=>qr|https?|}$/)
-	{
-		return 1;
-	}
-	return 0;
+sub validate_target {
+    my ( $obj, $param ) = get_param(@_);
+    my $target = $param->{'target'};
+    if ( $target =~ /^$RE{URI}{HTTP}{-scheme=>qr|https?|}$/ ) {
+        return 1;
+    }
+    return 0;
 }
 
-sub check_exists
-{
-	my ($obj, $param) = get_param(@_);
-	my $tag = lc($param->{'tag'});
-	unless ($obj->validate_tag(
-		'tag' => $tag,
-	)) {
-		die(qq|Invalid hashtag: '$tag'|);
-	}
-	return sqltable('ring_hashtag')->count(
-		'hashtag' => $tag,
-	);
+sub check_exists {
+    my ( $obj, $param ) = get_param(@_);
+    my $tag = lc( $param->{'tag'} );
+    unless ( $obj->validate_tag( 'tag' => $tag, ) ) {
+        die(qq|Invalid hashtag: '$tag'|);
+    }
+    return sqltable('ring_hashtag')->count( 'hashtag' => $tag, );
 }
 
-sub create
-{
-	my ($obj, $param) = get_param(@_);
-	my $tag = lc($param->{'tag'});
-	unless ($obj->validate_tag(
-		'tag' => $tag,
-	)) {
-		die(qq|Invalid hashtag: '$tag'|);
-	}
-	my $uid = $param->{'user_id'};
-	my $url = $param->{'target_url'};
-	my $expires = $param->{'expires'};
-	my $trec;
-	eval {
-		$trec = Note::Row::create('ring_hashtag', {
-			'hashtag' => $tag,
-			'user_id' => $uid,
-			'target_url' => $url,
-			'ts_expires' => $expires,
-		});
-	};
-	if ($@)
-	{
-		my $err = $@;
-		if ($err =~ /Duplicate/)
-		{
-			return undef;
-		}
-		else
-		{
-			die($err);
-		}
-	}
-	return $trec;
+sub create {
+    my ( $obj, $param ) = get_param(@_);
+    my $tag = lc( $param->{'tag'} );
+    unless ( $obj->validate_tag( 'tag' => $tag, ) ) {
+        die(qq|Invalid hashtag: '$tag'|);
+    }
+    my $uid     = $param->{'user_id'};
+    my $url     = $param->{'target_url'};
+    my $expires = $param->{'expires'};
+    my $trec;
+    eval { $trec = Note::Row::create( 'ring_hashtag', { 'hashtag' => $tag, 'user_id' => $uid, 'target_url' => $url, 'ts_expires' => $expires, } ); };
+    if ($@) {
+        my $err = $@;
+        if ( $err =~ /Duplicate/ ) {
+            return undef;
+        }
+        else {
+            die($err);
+        }
+    }
+    return $trec;
 }
 
-sub delete
-{
-	my ($obj, $param) = get_param(@_);
-	my $rc = new Note::Row(
-		'ring_hashtag' => {
-			'user_id' => $param->{'user_id'},
-			'id' => $param->{'id'},
-		},
-	);
-	if ($rc->id())
-	{
-		$rc->delete();
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
+sub delete {
+    my ( $obj, $param ) = get_param(@_);
+    my $rc = new Note::Row(
+        'ring_hashtag' => {
+            'user_id' => $param->{'user_id'},
+            'id'      => $param->{'id'},
+        },
+    );
+    if ( $rc->id() ) {
+        $rc->delete();
+        return 1;
+    }
+    else {
+        return 0;
+    }
 }
 
-sub update
-{
-	my ($obj, $param) = get_param(@_);
-	my $rc = new Note::Row(
-		'ring_hashtag' => {
-			'user_id' => $param->{'user_id'},
-			'id' => $param->{'id'},
-		},
-	);
-	if ($rc->id())
-	{
-		$rc->update({
-			'target_url' => $param->{'target'},
-		});
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
+sub update {
+    my ( $obj, $param ) = get_param(@_);
+    my $rc = new Note::Row(
+        'ring_hashtag' => {
+            'user_id' => $param->{'user_id'},
+            'id'      => $param->{'id'},
+        },
+    );
+    if ( $rc->id() ) {
+        $rc->update( { 'target_url' => $param->{'target'}, } );
+        return 1;
+    }
+    else {
+        return 0;
+    }
 }
 
-sub get_user_hashtags
-{
-	my ($obj, $param) = get_param(@_);
-	my $uid = $param->{'user_id'};
-	my $q = sqltable('ring_hashtag')->get(
-		'select' => ['id', 'hashtag', 'ts_expires as expires', 'target_url'],
-		'where' => {
-			'user_id' => $uid,
-		},
-		'order' => 'hashtag asc',
-#		'limit' => '10',
-	);
-	return $q;
+sub get_user_hashtags {
+    my ( $obj, $param ) = get_param(@_);
+    my $uid = $param->{'user_id'};
+    my $q   = sqltable('ring_hashtag')->get(
+        'select' => [ 'id', 'hashtag', 'ts_expires as expires', 'target_url' ],
+        'where' => { 'user_id' => $uid, },
+        'order' => 'hashtag asc',
+
+        #		'limit' => '10',
+    );
+    return $q;
 }
 
 1;
