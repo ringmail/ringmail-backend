2ee78a0c680c3a5e6522cd7f1faeda81ed9fe792
Arkadiy <arkadiy@dyl.com>
2016-05-18 01:47:33 +0000

    Make image upload work with image upload.

--- lib/Page/ring/setup/body_background_image.pm
+++ lib/Page/ring/setup/body_background_image.pm
@@ -6,6 +6,7 @@ use warnings;
 use Moose;
 use POSIX 'strftime';
 use JSON::XS;
+use Readonly;
 
 use Note::Page;
 use Note::Param;
@@ -15,6 +16,8 @@ use Ring::User;
 extends 'Note::Page';
 extends 'Page::ring::user';
 
+Readonly my $DAYS => 24 * 3_600;
+
 sub load {
     my ( @args, ) = @_;
 
@@ -22,20 +25,18 @@ sub load {
     my $uploads = $param->{request}->uploads();
     my $response;
     if ( exists $uploads->{'f_d1-body_background_image'} ) {
-        my $ul   = $uploads->{'f_d1-body_background_image'};
-        my $file = $ul->basename();
-        my $path = $ul->path();
+        my $file = $uploads->{'f_d1-body_background_image'}->path();
         my $s3   = 'Note::AWS::S3'->new(
             access_key => $::app_config->{s3_access_key},
             secret_key => $::app_config->{s3_secret_key},
         );
-        my $key = join q{/}, => 'body_background_image', $obj->user()->id(), $file;
+        my $key = join _ => 'ringpage', $obj->user()->id(), 'body_background_image.jpg';
         $s3->upload(
-            file         => $path,
+            file         => $file,
             key          => $key,
             bucket       => 'ringmail1',
             content_type => 'image/jpeg',
-            expires      => strftime( '%F', gmtime( time() + ( 24 * 3_600 * 1 ) ) ),
+            expires      => strftime( '%F', gmtime( time() + ( $DAYS * 1 ) ) ),
             acl_short    => 'public-read',
         );
         my $url = $s3->download_url(
@@ -43,13 +44,10 @@ sub load {
             bucket => 'ringmail1',
         );
         ::log( $url, );
-        $response = {
-            result => 'ok',
-            type   => 'image/jpeg',
-        };
+        $response = { files => [ { url => qq{$url}, }, ], };
     }
     else {
-        $response = { result => 'error', };
+        $response = { error => 'ERROR', };
     }
     $obj->response()->content_type( 'application/json', );
     return encode_json( $response, );
