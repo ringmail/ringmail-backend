d4416f21285838fbc62c50f264ccb265e511cbd2
Arkadiy <arkadiy@dyl.com>
2016-05-28 01:35:53 +0000

    Use template structure.

--- lib/Page/ring/setup/ringpage.pm
+++ lib/Page/ring/setup/ringpage.pm
@@ -55,16 +55,22 @@ around load => sub {
         $ringpage_row_data->{$key} = $value // $default;
     }
 
-    $content->{ringpage} = $ringpage_row_data;
-    ::log( $content->{ringpage}, );
-    $content->{edit} = ( $form->{edit} ) ? 1 : 0;
+    my $template_id        = $ringpage_row_data->{template_id};
+    my $template_row       = Note::Row->new( ring_template => { id => $template_id, }, );
+    my $template_row_data  = $template_row->data();
+    my $template_structure = decode_json $template_row_data->{structure};
 
     my $buttons = sqltable( 'ring_button', )->get(
         select => [ qw{ id button uri }, ],
         where  => { ringpage_id => $ringpage_row->id(), },
     );
 
-    $obj->content()->{ringpage}->{buttons} = $buttons;
+    $content->{structure}           = $template_structure;
+    $content->{ringpage}            = $ringpage_row_data;
+    $content->{ringpage}->{buttons} = $buttons;
+    $content->{edit}                = $form->{edit} ? 1 : 0;
+
+    ::log( $content->{template}, );
 
     return $obj->$next( $param, );
 };
@@ -107,8 +113,6 @@ sub edit {
 
             my $each_array = each_arrayref [ $obj->request()->parameters()->get_all( 'd1-button_id', ), ], [ $obj->request()->parameters()->get_all( 'd1-button_text', ), ], [ $obj->request()->parameters()->get_all( 'd1-button_link', ), ];
             while ( my ( $button_id, $button_text, $button_link, ) = $each_array->() ) {
-
-                ::log( $button_id, $button_text, $button_link, );
 
                 if ( $button_id eq q{} ) {
 
