05f8ef9434376c99b5fdb7841f00469165ac4bb1
Arkadiy <arkadiy@dyl.com>
2016-06-07 01:09:58 +0000

    Check yo hashtag initial commit.

--- lib/Page/ring/setup/check_hashtag.pm
+++ lib/Page/ring/setup/check_hashtag.pm
@@ -4,36 +4,34 @@ use strict;
 use warnings;
 
 use Moose;
-use JSON::XS 'encode_json';
-use Data::Dumper;
-use HTML::Entities 'encode_entities';
-use POSIX 'strftime';
 
-use Note::XML 'xml';
 use Note::Param;
 
 use Ring::User;
-use Ring::API;
 use Page::ring::user;
 
-extends 'Page::ring::user';
+use parent 'Page::ring::user';
 
-sub load {
+around load => sub {
+    my ( $next, @args, ) = @_;
+
+    my ( $self, $param ) = get_param( @args, );
+
+    return $self->$next( $param, );
+};
+
+sub check_hashtag {
     my ( @args, ) = @_;
 
     my ( $self, $param ) = get_param( @args, );
-    my $form = $self->form();
 
-    ::_log($form);
+    ::log( $self, $param, );
 
-    my $content    = $self->content();
-    my $user       = $self->user();
-    my $user_row   = $user->row();
-    my $user_login = $user_row->data( 'login', );
+    $self->form()->{hashtag} = $param->{hashtag};
 
-    $content->{email} = $user_login;
+    $self->value()->{available} = 1 if $param->{hashtag} eq 'foobar';
 
-    return $self->SUPER::load($param);
+    return;
 }
 
 1;
