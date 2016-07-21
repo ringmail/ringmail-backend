927520346f563840c71e2c72ebe6ffd6f39aaa5c
Arkadiy <arkadiy@dyl.com>
2016-07-21 02:01:46 +0000

    Commit DDL changes.

--- sql/changes/update-00002.sql
+++ sql/changes/update-00002.sql
@@ -1,4 +1,4 @@
 ALTER TABLE ring_hashtag ADD COLUMN active tinyint(1) NOT NULL DEFAULT 1,
 ADD COLUMN paid tinyint(1) NOT NULL DEFAULT 0;
 
-INSERT INTO sys_version SET id=1;
+INSERT INTO sys_version SET id=2;
