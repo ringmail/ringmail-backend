b5bef089f8e67e4b73c5461fa88b030305c33b4b
Arkadiy <arkadiy@dyl.com>
2016-07-27 20:18:29 +0000

    Add coupin table DDL.

--- sql/changes/update-00008.sql
+++ sql/changes/update-00008.sql
@@ -1,4 +1,13 @@
-ALTER TABLE coupon DROP COLUMN amount;
-ALTER TABLE coupon ADD UNIQUE code_1 (code);
+CREATE TABLE `coupon` (
+	  `id` bigint unsigned NOT NULL auto_increment,
+	  `code` varchar(8) NOT NULL DEFAULT '',
+	  `transaction_id` bigint unsigned NULL,
+	  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	  `user_id` bigint unsigned NULL,
+	  UNIQUE INDEX `code_1` (`code`),
+	  INDEX `transaction_id_1` (`transaction_id`),
+	  INDEX `user_id_1` (`user_id`),
+	  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO sys_version SET id=8;
