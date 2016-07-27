9a5b5a874b86393ab1f4d79c6bdc850984d65e71
Arkadiy <arkadiy@dyl.com>
2016-07-27 20:33:29 +0000

    Remove some DDL.

--- sql/changes/update-00002.sql
+++ sql/changes/update-00002.sql
@@ -1,4 +1,1 @@
-ALTER TABLE ring_hashtag ADD COLUMN active tinyint(1) NOT NULL DEFAULT 1,
-ADD COLUMN paid tinyint(1) NOT NULL DEFAULT 0;
-
 INSERT INTO sys_version SET id=2;
--- sql/changes/update-00006.sql
+++ sql/changes/update-00006.sql
@@ -1,8 +1,1 @@
-CREATE DEFINER=`root`@`localhost` EVENT `ring_cart` ON SCHEDULE EVERY 5 MINUTE STARTS '2016-07-25 00:00:00' ON COMPLETION NOT PRESERVE ENABLE DO DELETE FROM 
-	ring_cart 
-WHERE 
-	transaction_id IS NULL 
-	AND 
-	ts < NOW() - INTERVAL 2 HOUR;
-
 INSERT INTO sys_version SET id=6;
