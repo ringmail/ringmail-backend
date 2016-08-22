92839b8900ddbbe4c65c90db60689c19bf277030
Mike Frager <mfrager@dyl.com>
2016-08-22 23:32:12 +0000

    sql 16

---
+++ sql/changes/update-00016.sql
@@ -0,0 +1,5 @@
+ALTER TABLE ring_conversation DROP INDEX from_user_id_1,
+                              ADD COLUMN conversation_uuid varchar(36) NULL DEFAULT NULL,
+                              ADD UNIQUE conversation_uuid_1 (conversation_uuid),
+                              ADD UNIQUE from_user_id_1 (from_user_id, to_user_id);
+INSERT INTO sys_version SET id=16;
