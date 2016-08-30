f1b36d60ec8ae0883dbb3da7ef6517a99d5f5a86
Arkadiy <arkadiy@dyl.com>
2016-08-30 01:29:09 +0000

    Hashtags Directory work.

--- lib/Page/ring/setup/admin/directory.pm
+++ lib/Page/ring/setup/admin/directory.pm
@@ -1,7 +1,8 @@
 package Page::ring::setup::admin::directory;
 
-use Moose;
-use Note::Param;
+use Moose 'extends';
+use Note::Param 'get_param';
+use Note::SQL::Table 'sqltable';
 
 extends 'Page::ring::user';
 
@@ -12,6 +13,44 @@ sub load {
 
     my $content = $self->content();
     my $form    = $self->form();
+
+    my ( $page, ) = ( $form->{page} // 1 =~ m{ \A \d+ \z }xms, );
+
+    my $offset = ( $page * 10 ) - 10;
+
+    my $count = sqltable('ring_hashtag')->count();
+
+    my $hashtags = sqltable('ring_hashtag')->get(
+        select => [
+            qw{
+
+                ring_cart.hashtag_id
+                ring_cart.transaction_id
+                ring_hashtag.hashtag
+                ring_hashtag.id
+                ring_hashtag.ringpage_id
+                ring_hashtag.target_url
+                ring_hashtag_directory.ts_directory
+                ring_page.ringpage
+
+                },
+            'ring_hashtag_directory.id AS directory_id',
+        ],
+        table     => [ qw{ ring_hashtag }, ],
+        join_left => [
+
+            [ ring_cart              => 'ring_hashtag.id = ring_cart.hashtag_id', ],
+            [ ring_page              => 'ring_hashtag.ringpage_id = ring_page.id', ],
+            [ ring_hashtag_directory => 'ring_hashtag_directory.hashtag_id = ring_hashtag.id', ],
+
+        ],
+        order => qq{ring_hashtag.id LIMIT $offset, 10},
+    );
+
+    ::log( $hashtags, );
+
+    $content->{count}    = $count;
+    $content->{hashtags} = $hashtags;
 
     return $self->SUPER::load( $param, );
 }
