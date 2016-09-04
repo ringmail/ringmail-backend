3b2a14b7716866516ca6a0bcc408f7960de2c7dd
Mike Frager <mfrager@dyl.com>
2016-09-04 21:17:07 +0000

    revised trigger

--- sql/changes/update-00021.sql
+++ sql/changes/update-00021.sql
@@ -1,17 +1,11 @@
 DELIMITER // 
 
-CREATE DEFINER=`root`@`localhost` TRIGGER `ringmail`.`ring_hashtag_directory` AFTER DELETE ON `ringmail`.`ring_hashtag_directory` FOR EACH ROW 
+-- CREATE DEFINER=`root`@`localhost` TRIGGER `ringmail`.`ring_hashtag_directory` AFTER DELETE ON `ringmail`.`ring_hashtag_directory` FOR EACH ROW 
+CREATE TRIGGER `ringmail`.`ring_hashtag_directory` AFTER DELETE ON `ringmail`.`ring_hashtag_directory` FOR EACH ROW 
 begin
-	update 
-	ring_hashtag 
-	set 
-	directory = 0 
-	where 
-	id = old.hashtag_id;
+	update ring_hashtag set directory = 0 where id = old.hashtag_id;
 end
-
 // 
 
 DELIMITER ; 
-
 INSERT INTO sys_version SET id=21;
