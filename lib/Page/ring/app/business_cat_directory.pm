6244dab9dc83f9c7248076d01d8da77979d7c1aa
Mark Baxter <mbaxter@dyl.com>
2017-02-27 21:48:21 +0000

    biz hashtag backend start

---
+++ lib/Page/ring/app/business_cat_directory.pm
@@ -0,0 +1,190 @@
+package Page::ring::app::business_cat_directory;
+use strict;
+use warnings;
+
+use vars qw();
+
+use Moose;
+use JSON::XS 'encode_json';
+use Data::Dumper;
+use URI::Escape;
+use POSIX 'strftime';
+use MIME::Base64 'encode_base64';
+
+use Note::XML 'xml';
+use Note::Row;
+use Note::SQL::Table 'sqltable';
+use Note::Param;
+
+use Ring::User;
+
+extends 'Note::Page';
+
+no warnings 'uninitialized';
+
+
+sub load
+{
+	my ($obj, $param) = get_param(@_);
+	my $form = $obj->form();
+
+	my $parentIdIn = $form->{'parent'};
+
+	::log('business category directory', {%$form, 'password' => ''});
+	my $user = Ring::User::login(
+		'login' => $form->{'login'},
+		'password' => $form->{'password'},
+	);
+	my $res = {'result' => 'Unauthorized'};
+	if ($user)
+	{
+		my $pid = $form->{'parent'};
+		if ($pid =~ /^\d+$/)
+		{
+			if ($pid == 0)
+			{
+				my $dq = sqltable('business_category')->get(
+					'select' => [
+						'c1.id',
+						'c1.business_category_name',
+						'c1.parent',
+						'c1.factual_category_id',
+					],
+					'table' => 'business_category c1',
+					'where' => 'c1.parent IS NULL and c1.factual_category_id IS NULL',
+					'order' => 'business_category_name asc',
+				);
+
+				my @cat = ();
+				foreach my $c (@$dq)
+				{
+					next if ($c->{'business_category_name'} =~ /\(none\)/i);
+					my $catimg = 'hashtagdir.jpg';
+					push @cat, {
+						'type' => 'hashtag_category',
+						'name' => uc($c->{'business_category_name'}),
+						'id' => $c->{'id'},
+						'image_url' => $obj->url('path' => '/img/hashtag_categories/'. $catimg),
+					};
+				}
+				my @group = ([]);
+				my $max = 2;
+				foreach my $i (@cat)
+				{
+					if (scalar(@{$group[-1]}) == $max)
+					{
+						push @group, [];
+					}
+					push @{$group[-1]}, $i;
+				}
+				foreach my $i (0..$#group)
+				{
+					$group[$i] = {
+						'type' => 'hashtag_category_group',
+						'group' => $group[$i],
+					};
+				}
+				$res->{'directory'} = \@group;
+				$res->{'result'} = 'ok';
+				$obj->{'response'}->header('Cache-Control', 'max-age='. 3600);
+			}
+			else
+			{
+				my $dq = sqltable('business_category')->get(
+					'select' => [
+						'c1.id',
+						'c1.business_category_name',
+						'c1.parent',
+						'c1.factual_category_id',
+
+					],
+					'table' => 'business_category c1, business_category c2',
+					'join' => 'c2.id=c1.parent',
+					'where' => "c1.parent=$pid",
+					'order' => 'business_category_name asc',
+				);
+				my @cat = ();
+
+				if (@$dq) {
+
+					foreach my $c (@$dq)
+					{
+						my $catimg = 'hashtagdir.jpg';
+						push @cat, {
+							'type' => 'hashtag_category',
+							'name' => uc($c->{'business_category_name'}),
+							'id' => $c->{'id'},
+							'image_url' => $obj->url('path' => '/img/hashtag_categories/'. $catimg),
+						};
+					}
+					my @group = ([]);
+					my $max = 2;
+					foreach my $i (@cat)
+					{
+						if (scalar(@{$group[-1]}) == $max)
+						{
+							push @group, [];
+						}
+						push @{$group[-1]}, $i;
+					}
+					foreach my $i (0..$#group)
+					{
+						$group[$i] = {
+							'type' => 'hashtag_category_group',
+							'group' => $group[$i],
+						};
+					}
+					$res->{'directory'} = \@group;
+					$res->{'result'} = 'ok';
+					$obj->{'response'}->header('Cache-Control', 'max-age='. 3600);
+
+				}
+				else
+				{
+					my $catrc = new Note::Row('business_category' => {'id' => $pid});
+					if ($catrc->id())
+					{
+						my @cat = (
+							{
+								'type' => 'hashtag_category_header',
+								'name' => uc($catrc->data('business_category_name')),
+								'id' => $pid,
+							}
+						);
+						my $tq = sqltable('business_place_category')->get(
+							'select' => [
+								'bhp.hashtag',
+								'bpc.category_id',
+								'bpc.place_id',
+							],
+							'table' => 'business_place_category bpc, business_hashtag_place bhp',
+							'join' => 'bpc.place_id=bhp.place_id',
+							'where' => {
+								'bpc.category_id' => $pid,
+							},
+							'order' => 'bhp.hashtag',
+						);
+						my %seen = ();
+						foreach my $i (@$tq)
+						{
+							my $tag = '#'. $i->{'hashtag'};
+							push @cat, {
+								'type' => 'hashtag',
+								'label' => $tag,
+								'session_tag' => $tag,
+							};
+						}
+						$res->{'directory'} = \@cat;
+						$res->{'result'} = 'ok';
+					}
+				}
+			}
+	
+		}
+	}
+	$obj->{'response'}->content_type('application/json');
+	::log($res);
+	return encode_json($res);
+}
+
+1;
---
+++ lib/Page/ring/business.pm
@@ -0,0 +1,106 @@
+package Page::ring::business;
+
+use strict;
+use warnings;
+
+use Moose;
+use JSON::XS 'decode_json';
+
+use Note::Row;
+use Note::Param;
+use Note::SQL::Table 'sqltable';
+
+extends 'Note::Page';
+
+sub load
+{
+    my ($obj, $param) = get_param(@_);
+    my $form = $obj->form();
+    my $content = $obj->content();
+    
+    my $idIn = $form->{'id'};
+    my $hashtagIn = $form->{'hashtag'};
+    my $rec;
+    my $placeId;
+    my $hashtag;
+
+    if ($hashtagIn)
+    {
+        $hashtag = "#" . $hashtagIn;
+        my $hashtagplaces = sqltable('business_hashtag_place')->get(
+        'select' => 'place_id',
+        'where' => { 'hashtag' => $hashtagIn, },
+        'order' => 'id asc',
+        );
+        $placeId = ${$hashtagplaces->[0]}{'place_id'};
+        if ($placeId) {
+            $rec = new Note::Row('business_place' => {'id' => $placeId});
+        }
+    }
+
+    if ($idIn)
+    {
+        $placeId = $idIn; 
+        my $hashtagplaces = sqltable('business_hashtag_place')->get(
+        'select' => 'hashtag',
+        'where' => { 'place_id' => $placeId, },
+        'order' => 'id asc',
+        );
+        $hashtag = "#" . ${$hashtagplaces->[0]}{'hashtag'};    
+        $rec = new Note::Row('business_place' => {'id' => $placeId});
+    }
+    
+    if ($rec)
+    {
+        $content->{'record'} = 'true';
+        my $chainName = $rec->data('chain_name');
+        if ($chainName)
+        {
+            my $chainSocial = new Note::Row('business_chain_social' => {'chain_name' => $chainName});
+            $chainName =~ s/(\s|[^a-zA-Z0-9])//g;
+            $content->{'bodybg'} = $chainName;
+            my $logoImg = "./img/business/logo/$chainName.png";
+            my $logoRec = new Note::Row('business_logos' => {'name' => $chainName});
+            if ($logoRec->data('name'))
+            {
+                $content->{'logo'} = $logoImg;
+            }
+        }
+        
+        my $tel = $rec->data('tel');
+        $tel =~ s/^\+1(\d{3})(\d{3})(\d{4})$/($1) $2-$3/;
+
+        my $fax = $rec->data('fax');
+        $fax =~ s/^\+1(\d{3})(\d{3})(\d{4})$/($1) $2-$3/;
+
+        $content->{'hashtag'} = $hashtag;
+        $content->{'placeId'} = $placeId;
+
+        $content->{'address'} = $rec->data('address');
+        $content->{'address_extended'} = $rec->data('address_extended');
+        $content->{'country'} = $rec->data('country');
+        $content->{'email'} = $rec->data('email');
+        $content->{'factual_id'} = $rec->data('factual_id');
+        $content->{'fax'} = $fax;
+        $content->{'hours_display'} = $rec->data('hours_display');
+        $content->{'locality'} = $rec->data('locality');
+        $content->{'name'} = $rec->data('name');
+        $content->{'neighborhood'} = $rec->data('neighborhood');
+        $content->{'po_box'} = $rec->data('po_box');
+        $content->{'post_town'} = $rec->data('post_town');
+        $content->{'postcode'} = $rec->data('postcode');
+        $content->{'region'} = $rec->data('region');
+        $content->{'tel'} = $tel;
+
+        my $website = $rec->data('website');
+        $content->{'websiteLink'} = $website;
+        $website =~ s/^(http|https):\/\/www\.//;
+        $website =~ s/\s+$//;
+        $content->{'website'} = $website;
+    }
+  
+    return $obj->SUPER::load($param);
+}
+
+1;
+
